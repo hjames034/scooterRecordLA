172010ddb65a966e5c7627625a956b66bd8aae91
hjames034 <60247356+hjames034@users.noreply.github.com>
2022-06-05 09:39:53 -0700

    Update parseScooter.py

--- parseScooter.py
+++ parseScooter.py
@@ -6,12 +6,17 @@ def jsonReaderScooter(urlToOpen):
     with urllib.request.urlopen(urlToOpen) as url:
         data = json.loads(url.read().decode())
     retStr='lat,lon,isdisabled,time\n'
+    try:
+        with open('outputDataUpdated.csv','r') as reader:
+            if retStr in reader:
+                retStr=''
+    except:
+        pass
     for item in data["data"]["bikes"]:
         retStr+=str(item['lat'])+','+str(item['lon'])+','+str(item['is_disabled'])+','+str(dateTimeStr)+'\n'
-    with open('outputDataNew.csv', 'a+') as writer:
+    with open('outputDataUpdated.csv', 'a+') as writer:
         writer.writelines(retStr)
     return True
 url = ["https://mds.bird.co/gbfs/los-angeles/free_bikes","https://s3.amazonaws.com/lyft-lastmile-production-iad/lbs/lax/free_bike_status.json","https://la-gbfs.getwheelsapp.com/free_bike_status.json"]
 for i in url:
     jsonReaderScooter(i)
-
