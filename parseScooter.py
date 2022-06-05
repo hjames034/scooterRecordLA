2541569e3b8137b74b71cf37251bff61616dd052
hjames034 <60247356+hjames034@users.noreply.github.com>
2022-06-05 09:32:50 -0700

    Update parseScooter.py

--- parseScooter.py
+++ parseScooter.py
@@ -2,11 +2,16 @@ import urllib.request, json
 import pytz
 from datetime import datetime
 dateTimeStr=datetime.utcnow().replace(tzinfo=pytz.utc)
-with urllib.request.urlopen("https://mds.bird.co/gbfs/los-angeles/free_bikes") as url:
-    data = json.loads(url.read().decode())
-retStr='lat,lon,isdisabled,time\n'
-for item in data["data"]["bikes"]:
-    retStr+=str(item['lat'])+','+str(item['lon'])+','+str(item['is_disabled'])+','+str(dateTimeStr)+'\n'
-with open('outputData.csv', 'a+') as writer:
-    writer.writelines(retStr)
+def jsonReaderScooter(urlToOpen):
+    with urllib.request.urlopen(urlToOpen) as url:
+        data = json.loads(url.read().decode())
+    retStr='lat,lon,isdisabled,time\n'
+    for item in data["data"]["bikes"]:
+        retStr+=str(item['lat'])+','+str(item['lon'])+','+str(item['is_disabled'])+','+str(dateTimeStr)+'\n'
+    with open('outputDataNew.csv', 'a+') as writer:
+        writer.writelines(retStr)
+    return True
+url = ["https://mds.bird.co/gbfs/los-angeles/free_bikes","https://s3.amazonaws.com/lyft-lastmile-production-iad/lbs/lax/free_bike_status.json"]
+for i in url:
+    jsonReaderScooter(i)
 
